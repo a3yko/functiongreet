62e03886bab1e34296e93c072d1bf4cd7658cf16
Adrian Atanasov <adriansoccar@gmail.com>
2019-09-07 12:30:15 -0500

    Initial Commit

---
+++ functiongreet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  functiongreet
+//
+//  Created by Adrian Atanasov on 9/7/19.
+//  Copyright © 2019 Adrian Atanasov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
